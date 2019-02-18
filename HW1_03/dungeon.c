b290ed304c9738d6bd0249e3baf76e615e2ff167
Thomas Powell <tksp98@gmail.com>
2019-02-18 15:25:23 -0600

    Fixed stairs load and save

---
+++ HW1_03/dungeon.c
@@ -0,0 +1,397 @@
+#include "dungeon.h"
+#include "load_save.h"
+
+void free_dungeon(dungeon_t *dungeon)
+{
+    free(dungeon->upstrs);
+    free(dungeon->downstrs);
+    free(dungeon->rooms);
+}
+
+void place_stairs(dungeon_t *d)
+{
+    // d->numDownStrs = ((rand() % 2) + 1);
+    // d->numUpStrs = ((rand() % 2) + 1);
+    d->numDownStrs = 1;
+    d->numUpStrs = 1;
+    uint8_t x, y;
+    int i;
+    while (1)
+    {
+        x = rand() % (max_dimX - 1) + 1;
+        y = rand() % (max_dimY - 1) + 1;
+
+        if (d->hardness[y][x] == 0 && d->map[y][x] != '<' && d->map[y][x] != '@')
+        {
+            
+            d->downstrs = malloc(sizeof(location_t));
+            d->downstrs[0].x = x;
+            d->downstrs[0].y = y;
+
+            d->map[y][x] = '>';
+            d->hardness[y][x] = 0;
+            break;
+        }
+    }
+
+    i = 0;
+    while (1)
+    {
+        x = rand() % (max_dimX - 1) + 1;
+        y = rand() % (max_dimY - 1) + 1;
+
+        if (d->hardness[y][x] == 0 && d->map[y][x] != '>' && d->map[y][x] != '@')
+        {
+            
+            d->upstrs = malloc(sizeof(location_t));
+            d->upstrs[0].x = x;
+            d->upstrs[0].y = y;
+
+            d->map[y][x] = '<';
+            d->hardness[y][x] = 0;
+            break;
+        }
+    }
+    return;
+}
+
+void place_player(dungeon_t *d)
+{
+    uint8_t x, y;
+
+    while (1)
+    {
+        x = rand() % (max_dimX - 1) + 1;
+        y = rand() % (max_dimY - 1) + 1;
+
+        if (d->hardness[y][x] == 0 && d->map[y][x] == '.')
+        {
+            d->pc.x = x;
+            d->pc.y = y;
+            d->map[y][x] = '@';
+            break;
+        }
+    }
+    return;
+}
+
+/**
+ * Main function for dungeon.c.
+ */
+int main(int argc, char const *argv[]){
+    //Creates a random unique seed for the dungeon.
+    time_t t;
+    t = time(NULL);
+    int seed = t;
+    srand(seed);
+    
+    //Creates the dungeon struct.
+    dungeon_t dungeon;
+
+    //Creates the maximum number of upstairs and downstairs
+    // dungeon.numUpStrs = ((rand() % max_upstrs) + 1);
+    // dungeon.numDownStrs = ((rand() % max_downstrs) + 1);
+    // dungeon.upstrs = malloc(sizeof(stairs_t)*dungeon.numUpStrs);
+    // dungeon.downstrs = malloc(sizeof(stairs_t)*dungeon.numDownStrs);
+
+    action_t action;
+
+    int i;
+    for (i = 1; i < argc; i++) //check for flags
+    {
+        if (!strcmp(argv[i], "--save") || !strcmp(argv[i], "-s"))
+            action = (action != load) ? save : load_save;
+        else if (!strcmp(argv[i], "--load") || !strcmp(argv[i], "-l"))
+            action = (action != save) ? load : load_save;
+        else if (!strcmp(argv[i], "--seed") && (i + 1 < argc))
+            seed = atoi(argv[i + 1]); 
+        else if(!strcmp(argv[i], "-sl") || !strcmp(argv[i], "-ls")){
+            action = load_save;
+        }
+    }
+
+    switch (action)
+    {
+    case load:
+        printf("loading...\n");
+        load_dungeon(&dungeon);
+        break;
+    case save:
+        printf("saving...\n");
+        generate_dungeon(&dungeon);
+        save_dungeon(&dungeon);
+        break;
+    case load_save:
+        printf("loading and saving...\n");
+        load_dungeon(&dungeon);
+        save_dungeon(&dungeon);
+        break;
+    default:
+        generate_dungeon(&dungeon);
+        break;
+    }
+
+    printf("Seed: %d\n", seed);
+    print_dungeon(&dungeon);
+
+    free_dungeon(&dungeon);
+    return 0;
+}
+void generate_dungeon(dungeon_t *dungeon){
+    assign_border(dungeon);
+    create_rooms(dungeon);
+    create_corridors(dungeon);
+    assign_hardness(dungeon);
+    place_stairs(dungeon);
+    place_player(dungeon);
+}
+void print_dungeon(dungeon_t *dungeon){
+    int y, x;
+    for(y = 0; y < max_dimY; y++){
+        for(x = 0; x < max_dimX; x++){
+            printf("%c", dungeon->map[y][x]);
+        }
+        printf("\n");
+    }
+} 
+void assign_border(dungeon_t *dungeon){
+    int y, x;
+
+    for(int y = 0; y < max_dimY; y++){
+        for(int x = 0; x < max_dimX; x++){
+            if(y == 0 || y == max_dimY - 1){dungeon->map[y][x] = '-';}
+            else if(x == 0 || x == max_dimX - 1){dungeon->map[y][x] = '|';}
+            else {dungeon->map[y][x] = ' ';}
+        }
+    }
+}
+bool origin(dungeon_t *dungeon, int x, int y){
+    //Checks if x-y position is already occupied.
+    if(dungeon->map[y][x] == '|' || dungeon->map[y][x] == '-' || dungeon->map[y][x] == '.'){return false;}
+    //Checks for a clear one space margin so the origin doesn't overlap in another room.
+    else if (dungeon->map[y + 1][x] == '.' || dungeon->map[y - 1][x] == '.' || dungeon->map[y][x + 1] == '.' || dungeon->map[y][x - 1] == '.'){return false;}
+    else {return true;}
+    return true;
+}
+bool proximity(dungeon_t *dungeon, int startX, int startY, int length, int height){
+    if((startX + length) >= max_dimX || (startY + height) >= max_dimY){return false;}
+
+    int x, y;
+    for(y = startY; y < (startY + height); y++){
+        for(x = startX; x < (startX + length); x++){
+            if(dungeon->map[y][x] == '.' || dungeon->map[y + 1][x] == '.' || dungeon->map[y][x + 1] == '.' 
+            || dungeon->map[y - 1][x] == '.' || dungeon->map[y][x - 1] == '.'){return false;}
+        }
+    }
+    return true;
+}
+void assign_rooms(dungeon_t *dungeon, int startX, int startY, int length, int height){
+    int y, x;
+    for(y = startY; y < startY + height; y++){
+        for(x = startX; x < startX + length; x++){dungeon->map[y][x] = '.';}
+    }
+}
+int numOfRooms(){
+
+    bool decay = true;
+    double chance = 50;
+    int rooms = min_rms;
+    while(decay == true){
+        //Random out of 100.
+        double probability = (rand() % 101);
+        if(probability <= chance){
+            //Keeps decreasing the chance for additional rooms.
+            chance = chance / 2.0;
+            rooms += 1;
+            continue;
+        }
+        if(rooms == max_rms){decay = false;}
+        else{decay = false;}
+    }
+    return rooms;
+}
+void create_rooms(dungeon_t *dungeon){
+
+    //Creates the maximum number of rooms
+    dungeon->numRooms = numOfRooms();
+    
+    //Gets 6-10 rooms for the dungeon
+    
+    int totalRooms;
+    totalRooms = dungeon->numRooms;
+
+    //Initializes random origin points.
+    int randX, randY;
+    //Initializes random length and height.
+    int rLength, rHeight;
+    int roomDetails[dungeon->numRooms][4];
+
+    int attempts, r;
+
+    for(r = 0; r < totalRooms; r++){
+        attempts++;
+
+        randX = (rand() % (max_dimX + 1));
+        randY = (rand() % (max_dimY + 1));
+
+        rLength = (rand() % (max_rmX - min_rmX + 1) + min_rmX);
+        rHeight = (rand() % (max_rmY - min_rmY + 1) + min_rmY);
+        //Checks to see if the origin point is not open.
+        if(!origin(dungeon, randX, randY)){
+            //Breaks out if it is unable to place rooms after 1000 attempts and 6 rooms are placed.
+            if(attempts > 1000 && (r >= 6)){
+                break;
+            }
+            else{
+                r -= 1;
+                continue;
+            }
+        }
+        else{
+            //Checks to see if length and height of room will fit in space.
+            if(!proximity(dungeon, randX, randY, rLength, rHeight)){
+                
+                rLength = (rand() % (8 - min_rmX + 1) + min_rmX);
+                rHeight = (rand() % (6 - min_rmY + 1) + min_rmY);
+                //Checks to see if a smaller room will fit in the space.
+                if(!proximity(dungeon, randX, randY, rLength, rHeight)){
+                    r -= 1;
+                    continue;
+                }
+                //If it fits, assigns locations to '.' and saves origin location.
+                else{
+                    assign_rooms(dungeon, randX, randY, rLength, rHeight);
+                        roomDetails[r][1] = randY;
+                        roomDetails[r][2] = randX;
+                        roomDetails[r][3] = rHeight;
+                        roomDetails[r][4] = rLength;
+                }
+            }
+            //If it fits, assigns locations to '.' and saves origin location.
+            else{
+                assign_rooms(dungeon, randX, randY, rLength, rHeight);
+                    // dungeon->rooms[r].y = randY;
+                    // dungeon->rooms[r].x = randX;
+                    // dungeon->rooms[r].height = rHeight;
+                    // dungeon->rooms[r].length = rLength;
+
+                    roomDetails[r][1] = randY;
+                    roomDetails[r][2] = randX;
+                    roomDetails[r][3] = rHeight;
+                    roomDetails[r][4] = rLength;
+            }
+        }
+    }
+
+    dungeon->rooms = malloc(sizeof(room_t)*dungeon->numRooms);
+    int i;
+    for(i = 0; i < r; i++){
+        dungeon->rooms[i].y = roomDetails[i][1];
+        dungeon->rooms[i].x = roomDetails[i][2];
+        dungeon->rooms[i].height = roomDetails[i][3];
+        dungeon->rooms[i].length = roomDetails[i][4];
+    }
+
+    //Assigns player location starting point
+    dungeon->pc.y = dungeon->rooms[0].y;
+    dungeon->pc.x = dungeon->rooms[0].x;
+}
+void assign_corridor(dungeon_t *dungeon, uint8_t x, uint8_t y){
+
+    if (dungeon->map[y][x] == ' '){
+        dungeon->map[y][x] = '#';
+    }
+    return;
+}
+void create_corridors(dungeon_t *dungeon){
+
+    int i;
+    for (i = 0; i < dungeon->numRooms - 1; i++){
+        uint8_t startX, startY, endX, endY;
+
+        //start location
+        startX = dungeon->rooms[i].x + (rand() % dungeon->rooms[i].length);
+        startY = dungeon->rooms[i].y + (rand() % dungeon->rooms[i].height);
+
+        //end location
+        endX = dungeon->rooms[i + 1].x + (rand() % dungeon->rooms[i + 1].length);
+        endY = dungeon->rooms[i + 1].y + (rand() % dungeon->rooms[i + 1].height);
+
+        while ((startX != endX) || (startY != endY)){
+            int8_t moveX = (startX < endX) ? 1 : -1;
+            int8_t moveY = (startY < endY) ? 1 : -1;
+
+            uint8_t pos_x = startX + moveX;
+            uint8_t pos_y = startY + moveY;
+
+            float distance_x = sqrt(pow((endX - pos_x), 2) + pow(endY - startY, 2));
+            float distance_y = sqrt(pow((endX - startX), 2) + pow(endY - pos_y, 2));
+            float previous_d = sqrt(pow((endX - startX), 2) + pow(endY - startY, 2));
+
+            if (distance_x < previous_d && distance_y < previous_d){
+                if (dungeon->hardness[startY][pos_x] < dungeon->hardness[pos_y][startY]){
+                    startX += moveX;
+                }
+                else {
+                    startY += moveY;
+                }
+            }
+
+            else{
+                if (distance_x < distance_y){
+                    startX += moveX;
+                }
+                else{
+                    startY += moveY;
+                }
+            }
+
+            assign_corridor(dungeon, startX, startY);
+        }
+    }
+    return;
+}
+void create_stairs(dungeon_t *dungeon){
+    
+    dungeon->numUpStrs = ((rand() % max_upstrs) + 1);
+    dungeon->numDownStrs = ((rand() % max_downstrs) + 1);
+    dungeon->upstrs = malloc(sizeof(location_t)*dungeon->numUpStrs);
+    dungeon->downstrs = malloc(sizeof(location_t)*dungeon->numDownStrs);
+ 
+
+    int up = dungeon->numUpStrs;
+    int down = dungeon->numDownStrs;
+    int i = 0;
+    while(i != up){//Upstairs
+        int x = (rand() % (max_dimX + 1));
+        int y = (rand() % (max_dimY + 1));
+        if(dungeon->map[y][x] == '.' || dungeon->map[y][x] == '#'){
+            dungeon->map[y][x] = '<';
+            dungeon->upstrs[i].y = y;
+            dungeon->upstrs[i].x = x;
+            i++; 
+        }     
+    }     
+    i = 0; 
+    while(i != down){//Downstairs
+        int x = (rand() % (max_dimX + 1));
+        int y = (rand() % (max_dimY + 1));
+        if(dungeon->map[y][x] == '.' || dungeon->map[y][x] == '#'){
+            dungeon->map[y][x] = '>';
+            dungeon->downstrs[i].y = y;
+            dungeon->downstrs[i].x = x;
+            i++;
+        }
+    }
+}
+void assign_hardness(dungeon_t *dungeon){
+    int y, x;
+    for(y = 0; y < max_dimY; y++){
+        for(x = 0; x < max_dimX; x++){
+            if(dungeon->map[y][x] == '-' || dungeon->map[y][x] == '|'){dungeon->hardness[y][x] = 255;} //Immutable Border
+            else if(dungeon->map[y][x] == '#'){dungeon->hardness[y][x] = 0;}                      //Corridor
+            else if(dungeon->map[y][x] == '.'){dungeon->hardness[y][x] = 0;}                      //Downstairs
+            else{dungeon->hardness[y][x] = (rand() % 254) + 1;}                                             //Rock
+        }
+    }
+} 
---
+++ HW1_03/load_save.c
@@ -0,0 +1,283 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dungeon.h"
+#include "load_save.h"
+
+void fill_map(dungeon_t *dungeon)
+{
+    uint8_t i, j;
+
+    dungeon->numRooms = 0;
+
+    for (i = 0; i < max_dimY; i++) //height
+    {
+        for (j = 0; j < max_dimX; j++) //width
+        {
+            if (i == 0 || i == max_dimY - 1) //y borders
+            {
+                dungeon->map[i][j] = '-';
+                dungeon->hardness[i][j] = 255;
+            }
+            else if (j == 0 || j == max_dimX - 1) //x borders
+            {
+                dungeon->map[i][j] = '|';
+                dungeon->hardness[i][j] = 255;
+            }
+            else
+            {
+                dungeon->map[i][j] = ' ';
+                dungeon->hardness[i][j] = (rand() % 254) + 1;
+            }
+        }
+    }
+}
+
+int load_dungeon(dungeon_t *dungeon)
+{
+    FILE *f = NULL;
+
+    fill_map(dungeon);
+    open_file(&f, read);
+    char marker[13];
+    uint32_t version_marker;
+    uint32_t file_size;
+    uint16_t i, j, k;
+
+    fread(&marker, sizeof(char), 12, f);
+    printf("%s\n", marker);
+    fread(&version_marker, sizeof(uint32_t), 1, f);
+    printf("%d\n", be16toh(version_marker));
+    marker[12] = '\0';
+    if (strcmp(marker, MARK)) //check file marker
+        return -1;
+
+    if (be32toh(version_marker) != 0)
+        return -1;
+
+    fread(&file_size, sizeof(uint32_t), 1, f);
+    printf("%d\n", be32toh(file_size));
+    fread(&dungeon->pc.x, sizeof(uint8_t), 1, f);
+    fread(&dungeon->pc.y, sizeof(uint8_t), 1, f);
+    printf("Player: %d, %d\n", dungeon->pc.y, dungeon->pc.x);
+
+   // printf("X: %d, Y: %d\n", dungeon->pc.x, dungeon->pc.y);
+    fread(&dungeon->hardness, sizeof(uint8_t), 1680, f);
+    fread(&dungeon->numRooms, sizeof(uint16_t), 1, f);
+    dungeon->numRooms = be16toh(dungeon->numRooms);
+    //printf("%d\n", dungeon->numRooms);
+    dungeon->rooms = malloc(sizeof(room_t) * dungeon->numRooms);
+
+    //rooms
+    for (i = 0; i < dungeon->numRooms; i++)
+    {
+        fread(&dungeon->rooms[i].x, sizeof(uint8_t), 1, f);
+        fread(&dungeon->rooms[i].y, sizeof(uint8_t), 1, f);
+        fread(&dungeon->rooms[i].length, sizeof(uint8_t), 1, f);
+        fread(&dungeon->rooms[i].height, sizeof(uint8_t), 1, f);
+
+        //printf("%d. X: %d,Y: %d,Width: %d,Height: %d\n",i, dungeon->rooms[i].x, dungeon->rooms[i].y, dungeon->rooms[i].length, dungeon->rooms[i].height);
+
+        for (j = dungeon->rooms[i].y; j < dungeon->rooms[i].height + dungeon->rooms[i].y; j++) //add to map
+        {
+            for (k = dungeon->rooms[i].x; k < dungeon->rooms[i].length + dungeon->rooms[i].x; k++)
+            {
+                dungeon->map[j][k] = '.';
+            }
+        }
+    }
+    printf("# of rooms: %d\n", dungeon->numRooms);
+
+    // uint16_t numberOfMaxUpStairs;
+
+
+    // fread(&numberOfMaxUpStairs, 2, 1, f);
+    // be16toh(numberOfMaxUpStairs);
+    // dungeon->upstrs = malloc(numberOfMaxUpStairs * sizeof(location_t));
+    // printf("Up: %d\n", numberOfMaxUpStairs);
+    // for (i = 0; i < numberOfMaxUpStairs; i++){
+    //     fread(&dungeon->upstrs[i].x, 1, 1, f);
+    //     fread(&dungeon->upstrs[i].y, 1, 1, f);
+    //     if(dungeon->hardness[dungeon->upstrs[i].y][dungeon->upstrs[i].x] == 0){
+    //         dungeon->map[dungeon->upstrs[i].y][dungeon->upstrs[i].x] = '<';
+    //     }
+    //     printf("Num %d: X:%d, Y:%d\n", i, dungeon->upstrs[i].x, dungeon->upstrs[i].y);
+    // }
+
+    // uint16_t numberOfMaxDownStairs;
+
+    // fread(&numberOfMaxDownStairs, 2, 1, f);
+    // be16toh(numberOfMaxDownStairs);
+    // dungeon->downstrs = malloc(numberOfMaxDownStairs * sizeof(location_t));
+    // printf("Down: %d\n", numberOfMaxDownStairs);
+    // for (i = 0; i < numberOfMaxDownStairs; i++){
+    //     fread(&dungeon->downstrs[i].x, 1, 1, f);
+    //     fread(&dungeon->downstrs[i].y, 1, 1, f);
+    //     if(dungeon->hardness[dungeon->downstrs[i].y][dungeon->downstrs[i].x] == 0){
+    //         dungeon->map[dungeon->downstrs[i].y][dungeon->downstrs[i].x] = '>';
+    //     }
+    //     printf("Num %d: X:%d, Y:%d\n", i, dungeon->downstrs[i].x, dungeon->downstrs[i].y);
+    // }
+    
+    //Stairs
+    uint16_t num_stairs;
+    uint8_t x, y;
+
+    fread(&num_stairs, 2, 1, f);
+    num_stairs = be16toh(num_stairs);
+    for (; num_stairs; num_stairs--) {
+        fread(&x, 1, 1, f);
+        fread(&y, 1, 1, f);
+        dungeon->map[y][x] = '<';
+    }
+
+    fread(&num_stairs, 2, 1, f);
+    num_stairs = be16toh(num_stairs);
+    for (; num_stairs; num_stairs--) {
+        fread(&x, 1, 1, f);
+        fread(&y, 1, 1, f);
+        dungeon->map[y][x] = '>';
+    }
+
+
+    //upstairs
+
+    // fread(&dungeon->numUpStrs, sizeof(uint16_t), 1, f);
+    // dungeon->numUpStrs = be16toh(dungeon->numUpStrs);
+
+    // printf("Load Up: %d\n", dungeon->numUpStrs);
+
+    // dungeon->upstrs = malloc(sizeof(location_t) * dungeon->numUpStrs);
+    // for (i = 0; i < dungeon->numUpStrs; i++)
+    // {
+    //     fread(&dungeon->upstrs[i].x, sizeof(uint8_t), 1, f);
+    //     fread(&dungeon->upstrs[i].y, sizeof(uint8_t), 1, f);
+    //     printf("X:%d, Y:%d\n", dungeon->upstrs[i].x, dungeon->upstrs[i].y);
+    //     //add to map
+    //     dungeon->map[dungeon->upstrs[i].y][dungeon->upstrs[i].x] = '<';
+    // }
+    //dungeon->map[14]S[15] = '<';
+
+    // //downstairs
+    
+    // fread(&dungeon->numDownStrs, sizeof(uint16_t), 1, f);
+    // dungeon->numDownStrs = be16toh(dungeon->numDownStrs);
+    // printf("Load Down: %d\n", dungeon->numDownStrs);
+    // //printf("%d\n", dungeon->numDownStrs);
+    // dungeon->downstrs = malloc(sizeof(location_t) * dungeon->numDownStrs);
+    // for (i = 0; i < dungeon->numDownStrs; i++)
+    // {
+    //     fread(&dungeon->downstrs[i].x, sizeof(uint8_t), 1, f);
+    //     fread(&dungeon->downstrs[i].y, sizeof(uint8_t), 1, f);
+    //     //add to map
+    //     dungeon->map[dungeon->downstrs[i].y][dungeon->downstrs[i].x] = '>';
+    // }
+
+    //Corridors
+    for (i = 0; i < max_dimY; i++)
+    {
+        for (j = 0; j < max_dimX; j++)
+        {
+            if (dungeon->hardness[i][j] == 0 && dungeon->map[i][j] == ' ')
+            {
+                dungeon->map[i][j] = '#';
+            }
+        }
+    }
+
+    fclose(f);
+    printf("Finished Reading.\n");
+
+    return 0;
+}
+
+int save_dungeon(dungeon_t *dungeon)
+{
+    FILE *f = NULL;
+
+    //init values and convert endiness
+    uint32_t mark_v = 0;
+    mark_v = htobe32(mark_v);
+    uint32_t file_size = 1708 + dungeon->numRooms * 4 + dungeon->numUpStrs * 2 + dungeon->numDownStrs * 2;
+    file_size = htobe32(file_size);
+    uint16_t numRooms = dungeon->numRooms;
+    numRooms = htobe16(numRooms);
+    uint16_t numUpStrs = dungeon->numUpStrs;
+    numUpStrs = htobe16(numUpStrs);
+    uint16_t numDownStrs = dungeon->numDownStrs;
+    numDownStrs = htobe16(numDownStrs);
+    printf("Up:%d, Down:%d\n", numUpStrs, numDownStrs);
+
+
+    open_file(&f, write);                                   //open file
+
+    fwrite(&MARK, 12, 1, f);                                //file marker
+    fwrite(&mark_v, sizeof(uint32_t), 1, f);                //file version
+    fwrite(&file_size, sizeof(uint32_t), 1, f);             //file size
+    fwrite(&dungeon->pc.x, sizeof(uint8_t), 1, f);            //player x
+    fwrite(&dungeon->pc.y, sizeof(uint8_t), 1, f);            //player y
+    fwrite(&dungeon->hardness, sizeof(uint8_t), 1680, f);         //hardness array
+
+    //rooms
+    int i;
+    fwrite(&numRooms, sizeof(uint16_t), 1, f);             //number of room
+    for (i = 0; i < dungeon->numRooms; i++)
+    {
+        fwrite(&dungeon->rooms[i].x, sizeof(uint8_t), 1, f);      //room x
+        fwrite(&dungeon->rooms[i].y, sizeof(uint8_t), 1, f);      //room y
+        fwrite(&dungeon->rooms[i].length, sizeof(uint8_t), 1, f);  //room width
+        fwrite(&dungeon->rooms[i].height, sizeof(uint8_t), 1, f); //room height
+    }
+
+    //upstairs
+    fwrite(&numUpStrs, sizeof(uint16_t), 1, f);          //number of up stairs
+    printf("Save Up: %d\n", dungeon->numUpStrs);
+    for (i = 0; i < dungeon->numUpStrs; i++)
+    {
+        fwrite(&dungeon->upstrs[i].x, sizeof(uint8_t), 1, f);   //upstair x
+        fwrite(&dungeon->upstrs[i].y, sizeof(uint8_t), 1, f);   //upstair y
+        printf("%d. %d, %d\n", i, dungeon->upstrs[i].y, dungeon->upstrs[i].x);
+    }
+
+    //downstairs
+    fwrite(&numDownStrs, sizeof(uint16_t), 1, f);        //num of down stairs
+    printf("Save Down: %d\n", dungeon->numDownStrs);
+    for (i = 0; i < dungeon->numDownStrs; i++)
+    {
+        fwrite(&dungeon->downstrs[i].x, sizeof(uint8_t), 1, f); //downstair x
+        fwrite(&dungeon->downstrs[i].y, sizeof(uint8_t), 1, f); //downstair y
+        printf("%d. %d, %d\n", i, dungeon->downstrs[i].y, dungeon->downstrs[i].x);
+    }
+
+    fclose(f);                                              //close file
+    printf("Finished Writing.\n");                  
+    return 0;
+}
+
+int open_file(FILE **f, open_t open)
+{
+    char *path;
+
+    char *home = getenv("HOME");
+
+    if (!(path = malloc(strlen(home) + strlen("/.rlg327/dungeon") + 1)))
+        return 1;
+
+    strcpy(path, home);
+    strcat(path, "/.rlg327/dungeon");
+
+    switch (open)
+    {
+    case read:
+        if (!(*f = fopen(path, "r")))
+            return 1;
+        break;
+    case write:
+        if (!(*f = fopen(path, "w")))
+            return 1;
+        break;
+    }
+
+    free(path);
+    return 0;
+}
